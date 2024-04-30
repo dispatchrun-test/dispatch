d244610a20716778bfd28ca04cb32875cbfb238e
Achille Roussel <achille.roussel@gmail.com>
2024-04-29 21:14:35 -0700

    add dependency
    Signed-off-by: Achille Roussel <achille.roussel@gmail.com>

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/stealthrocket-test/dispatch
 
 go 1.22.0
+
+require github.com/stealthrocket-test/wzprof v0.1.0
--- main.go
+++ main.go
@@ -1,5 +1,8 @@
 package main
 
+import "github.com/stealthrocket-test/wzprof"
+
 func main() {
+	wzprof.TODO()
 	println("Hello, World!")
 }
